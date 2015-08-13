b6fa902dc0f3191f1ef8226df93645278c0cf659
Meryl <macdurst@imac-Meryl.local>
2015-08-13 19:35:00 +0200

    Debug

--- display.js
+++ display.js
@@ -1,28 +1,1 @@
-var p = document.createElement("p");
-var buttonHtml = "<a href=\"deleteSong.html?testmessage\"><img src = \"dust.png\" style = \"width : 20px; height : 20git pull
-px;\"></a>"
-
-
-function deleteL()
-{
-	alert("arg.type");
-}
-
-chrome.storage.sync.get('pl', function(result) 
-{
-	var rawFav = ""
-	rawFav = '<table border="1"><tr><th>Titre</th><th>Artiste</th><th>Album</th><th>Annee</th></tr>'+result.pl+"</table>"
-	rawFav = rawFav.split("__").join("</td><td>");
-	rawFav = rawFav.split(";new;").join("<tr><td>");
-	rawFav = rawFav.split(";end;").join('</td><td>'+buttonHtml+'</td></tr>');
-	document.getElementById('idData').innerHTML = rawFav
-});
-
-document.addEventListener('DOMContentLoaded', function () {
-      document.getElementById('deleteLine').addEventListener('click', deleteL); 
-}); 
-   
-document.ready(function(){alert("alert rouge")});
-
-
-
+﻿var p = document.createElement("p");var buttonHtml = "<a href=\"deleteSong.html?testmessage\"><img src = \"dust.png\" style = \"width : 20px; height : 20px;\"></a>"function deleteL(){	alert("arg.type");}chrome.storage.sync.get('pl', function(result) {	var rawFav = ""	rawFav = '<table border="1"><tr><th>Titre</th><th>Artiste</th><th>Album</th><th>Annee</th></tr>'+result.pl+"</table>"	rawFav = rawFav.split("__").join("</td><td>");	rawFav = rawFav.split(";new;").join("<tr><td>");	rawFav = rawFav.split(";end;").join('</td><td>'+buttonHtml+'</td></tr>');	document.getElementById('idData').innerHTML = rawFav});document.addEventListener('DOMContentLoaded', function () {      document.getElementById('deleteLine').addEventListener('click', deleteL); });    document.ready(function(){alert("alert rouge")});
--- popup.js
+++ popup.js
@@ -1,61 +1,1 @@
-// Copyright (c) 2014 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-document.addEventListener('DOMContentLoaded', function () {
-      document.getElementById('disp').addEventListener('click', display); 
-      document.getElementById('add').addEventListener('click', save);            
-      document.getElementById('openPlayer').addEventListener('click',openPlayer); 
-});
-
-
-function httpGet(theUrl)
-{
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false );
-    xmlHttp.send( null );
-    return xmlHttp.responseText;
-}
-
-
-
-function save() {
-    var httpRaw=httpGet("http://www.fipradio.fr/sites/default/files/import_si/si_titre_antenne/FIP_player_current.json")
-    var json = JSON.parse(httpRaw);
-    var album= json.current.song.titreAlbum
-    var titre = json.current.song.titre
-    var artiste = json.current.song.interpreteMorceau
-    var annee = json.current.song.anneeEditionMusique
-    chrome.storage.sync.get('pl', function(result) 
-    {	
-	var playlist = ""
-	if (result.pl){playlist = result.pl}
-	playlist=playlist+";new;"+titre+"__"+ artiste+"__"+album+"__"+annee+";end;"
-	chrome.storage.sync.set({'pl' : playlist}, function() {});
-    });
-    //alert("ajoute!")
-}
-
-function display() {
-    
-    
-    chrome.tabs.create({url: "display.html"});
-    
-    
-    var httpRaw=httpGet("http://www.fipradio.fr/sites/default/files/import_si/si_titre_antenne/FIP_player_current.json")
-    var json = JSON.parse(httpRaw);
-    var album= json.current.song.titreAlbum
-    var titre = json.current.song.titre
-    var artiste = json.current.song.interpreteMorceau
-    var annee = json.current.song.anneeEditionMusique
-    chrome.storage.sync.get('pl', function(result) 
-    {
-        var playlist = result.pl
-        alert("playlist="+playlist)
-    });
-}
-
-function openPlayer()
-{
-    chrome.tabs.create({url: "http://www.fipradio.fr/player"});
-}
+﻿// Copyright (c) 2014 The Chromium Authors. All rights reserved.// Use of this source code is governed by a BSD-style license that can be// found in the LICENSE file.document.addEventListener('DOMContentLoaded', function () {      document.getElementById('disp').addEventListener('click', display);       document.getElementById('add').addEventListener('click', save);                  document.getElementById('openPlayer').addEventListener('click',openPlayer); });function httpGet(theUrl){    var xmlHttp = new XMLHttpRequest();    xmlHttp.open( "GET", theUrl, false );    xmlHttp.send( null );    return xmlHttp.responseText;}function save() {    var httpRaw=httpGet("http://www.fipradio.fr/sites/default/files/import_si/si_titre_antenne/FIP_player_current.json")    var json = JSON.parse(httpRaw);    var album= json.current.song.titreAlbum    var titre = json.current.song.titre    var artiste = json.current.song.interpreteMorceau    var annee = json.current.song.anneeEditionMusique    chrome.storage.sync.get('pl', function(result)     {		var playlist = ""	if (result.pl){playlist = result.pl}	playlist=playlist+";new;"+titre+"__"+ artiste+"__"+album+"__"+annee+";end;"	chrome.storage.sync.set({'pl' : playlist}, function() {});    });    alert("ajoute")}function display() {            chrome.tabs.create({url: "display.html"});       var httpRaw=httpGet("http://www.fipradio.fr/sites/default/files/import_si/si_titre_antenne/FIP_player_current.json")    var json = JSON.parse(httpRaw);    var album= json.current.song.titreAlbum    var titre = json.current.song.titre    var artiste = json.current.song.interpreteMorceau    var annee = json.current.song.anneeEditionMusique    chrome.storage.sync.get('pl', function(result)     {        var playlist = result.pl    });}function openPlayer(){    chrome.tabs.create({url: "http://www.fipradio.fr/player"});}
