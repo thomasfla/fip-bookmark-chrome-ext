072d05de90d1f258bb5a7430989d1587e316facb
fbailly <fbailly@laas.fr>
2017-01-20 09:57:32 +0100

    Derniere mise a jour

--- currentSong.js
+++ currentSong.js
@@ -13,12 +13,11 @@ var album= json.current.song.titreAlbum
 var titre = json.current.song.titre
 var artiste = json.current.song.interpreteMorceau
 var annee = json.current.song.anneeEditionMusique
-var visuel = json.current.song.visuel.small
+var visuel = "http://is4.mzstatic.com/image/thumb/Music6/v4/cd/97/0a/cd970adf-ed41-8993-1a4f-8ac894d14683/source/100x100bb.jpg"
 
 var p = document.createElement("p");
 document.getElementById('idCurrentSong').innerHTML = 
 "<table><tr><td>"+"<img src="+visuel+"/>"+
 "</td><td><table><tr><td><b><font size = '3'>"+titre+"</font></b></td></tr><tr><td><font size = '2'>"+
-artiste+"</font></td></tr><tr><td><font size = '2'>"+album+"</font></td></tr><tr><td><font size = '1'>"+
-annee+"</font></td></tr></table></td></tr></table>"
+artiste+"</font></td></tr><tr><td><font size = '2'>"+album+"</font></td></tr><tr><td><font size = '1'>"+annee+"</font></td></tr></table></td></tr></table>"
 
